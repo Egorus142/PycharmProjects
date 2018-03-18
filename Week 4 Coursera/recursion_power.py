780d1cf796b3114081ccbb112758e82637f31a5a
Egorus142 <kuzn777@mail.ru>
2018-03-18 22:02:26 +0300

    New task

---
+++ Week 4 Coursera/recursion_power.py
@@ -0,0 +1,17 @@
+a = a_init = float(input())
+n = int(input())
+
+
+def power(a, n):
+    if n == 0:
+        return(print(1))
+    if n != 1:
+        if n > 0:
+            a *= a_init
+            n -= 1
+        power(a, n)
+    else:
+        print(a)
+
+
+power(a, n)
