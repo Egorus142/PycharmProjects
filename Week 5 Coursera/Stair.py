00606f8026fbcbbc5446073ed2fa99b845662a11
Egorus142 <kuzn777@mail.ru>
2018-04-12 02:18:09 +0300

    some progress with stair task

--- Week 5 Coursera/Stair.py
+++ Week 5 Coursera/Stair.py
@@ -1,7 +1,7 @@
 a = int(input())
 
-for number in range(1, a + 1):
-    print(number)
-
-
-    # Срез в цикле
+for i in range(1, a + 1):
+    for j in range(i, a + 1):
+        print(i, end='')
+        i += 1
+    print()
